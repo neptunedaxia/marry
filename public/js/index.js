4e2f079f4427c2987dda49ca9d0b1b87b987a56b
周知 <zhouzhi@zhouzhideMacBook-Pro.local>
2015-07-22 22:57:47 +0800

    submit code

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,23 @@
 ;Zepto(function($){
 
-    var img = new Image(); 
-    addSrc(img, "img-1.jpg");
-    var type = getFileSize(img);
+    // var img = new Image(); 
+    // addSrc(img, "img-1.jpg");
+    // $(img).bind("load",function(e){
+    //     var type = getFileSize(this);
+    //     if(type = "w"){
+
+    //     }else
+    // });
+    
+    if(window.orientation == 0 || window.orientation == 180){
+
+    }else if(window.orientation == 90 || window.orientation == -90){
+
+    }
 
     $("img").attr("src", "img-1.jpg");
+
+    window.addEventListener("onorientationchange" in window ? "orientationchange" : "resize", orientationChange, false); 
 });
 
 function getFileSize(image){ 
@@ -21,3 +34,14 @@ function addSrc(image, filePath){
         image.src = filePath;
     }
 }
+
+function orientationChange(){ 
+    var img  = $("img");
+    console.log(img);
+    switch(window.orientation) { 
+        case 0: break; 
+        case 180: break;
+        case -90: break; 
+        case 90: break;  
+    } 
+} 
